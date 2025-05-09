6bcf06e8c9034a005913cfb48aa802aa8fc5fcf3
Benjamin <benzilla@me.com>
2025-05-08 22:37:37 -0700

    Initial Commit

---
+++ Screener/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Screener
+//
+//  Created by Benjamin Zweig on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Screener/ScreenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenerApp.swift
+//  Screener
+//
+//  Created by Benjamin Zweig on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
